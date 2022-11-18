f51cd77c16bc47aff4515bc03e2d1952c154cc0b
ros9523 <rofidia95@gmail.com>
2022-11-17 22:32:50 -0600

    test

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Form from "./components/Form";
+import Logo from "./images/Logo.png";
+import Images from "./images/Images.png";
+
+function App() {
+  return (
+    <div className="App">
+      <img className="App_logo" src={Logo} />
+      <div className="App_Elements">
+        <Form></Form>
+        <img className="image" src={Images} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+
+import "../components/styles/form.css";
+
+const Form = () => {
+
+  
+  return (
+    <div className="container">
+      <form className="form">
+        <h1 className="form_title">Completa tu Registro</h1>
+        <div></div>
+        <div className="form_names">
+          <input
+            className="input"
+            type="text"
+            id="name"
+            name="Name"
+            placeholder="Nombre"
+          />
+          <input
+            className="input"
+            type="text"
+            id="Apellido"
+            name="Apellido"
+            placeholder="Apellido"
+          />
+        </div>
+        <div className="form_pass">
+          <i className="bx bx-lock-alt"></i>
+          <input
+            className="input input--pass"
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Contraseña"
+          />
+        </div>
+        <div className="form_pass">
+          <i className="bx bx-lock-alt"></i>
+          <input
+            className="input input--pass"
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Ingresa nuevamente"
+          />
+        </div>
+        <p className="text">
+          Password must be at least 8 characters and contain at least two of the
+          following: uppercase letters, lowevercase letters, numbers ans symbols
+        </p>
+        <div className="form_checks">
+          <li>
+            <input className="checkbox" type="checkbox" />
+            <label className="checkbox_label" for="logged in">
+              Keep me logged in
+            </label>
+          </li>
+          <li>
+            {" "}
+            <input className="checkbox" type="checkbox" />
+            <label className="checkbox_label" for="newsletters">
+              Sign me up for newsletters
+            </label>
+          </li>
+
+          <li>
+            <input className="checkbox" type="checkbox" />
+            <label className="checkbox_label" for="Registrate con">
+              Sign me up for newsletters
+            </label>
+          </li>
+        </div>
+        <button className="btn_submit">Registrate</button>
+        <div className="form_texts">
+          <p className="form_text">
+            YA TENES CUENTA? <spam className="form_spam">Ingresá</spam>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
